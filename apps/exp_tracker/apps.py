fde35715528a7a18ce470f8bb42644f3063f270f
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2025-12-29 16:55:41 +0000

    Initial project setup and added models

---
+++ apps/exp_tracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExpTrackerConfig(AppConfig):
+    name = 'apps.exp_tracker'
---
+++ apps/exp_tracker/models.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.db import models 
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Expense(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="expenses"
+    )
+    
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name="expense"
+    )
+    
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255, blank=False)
+    is_recurring = models.BooleanField(default=False)
+    expense_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user', '-expense_date'])
+        ]
+        
+    
+    def __str__(self):
+        return f"{self.user} - {self.amount}"
+
+    def is_high_value(self):
+        return self.amount > 5000
