5040d6e86d5239044c097fdcb4cb1810aa1868e8
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2025-12-31 14:50:20 +0000

    Added new application for api practice and just added router and simple APIView

---
+++ apps/expenses/views.py
@@ -0,0 +1,10 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HealthAPIView(APIView):
+    def get(self, request):
+        return Response({
+            "status": "ok",
+            "message": "Fresh DRF project is working"
+        })
--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.exp_tracker',
+    'apps.expenses',
     'rest_framework',
 ]
 
