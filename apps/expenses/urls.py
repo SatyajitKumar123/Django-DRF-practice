c7d550b1202d7d298ae2eaf387c14ddde8aabf25
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-01 17:07:40 +0000

    Added generic ListCreateAPIView and modified routes

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HealthAPIView
+# from .views import HealthAPIView
+from .views import ExpenseListCreateAPIView
 
 urlpatterns = [
-    path("health/", HealthAPIView.as_view()),
+    path("health/", ExpenseListCreateAPIView.as_view()),
 ]
--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -1,27 +1,33 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
+# from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Expense
 from .serializers import ExpenseSerializer
 
 
-class HealthAPIView(APIView):
-    def get(self, request):
-        expenses = Expense.objects.all()
-        serializer = ExpenseSerializer(expenses, many=True)
-        return Response(serializer.data)
+# class HealthAPIView(APIView):
+#     def get(self, request):
+#         expenses = Expense.objects.all()
+#         serializer = ExpenseSerializer(expenses, many=True)
+#         return Response(serializer.data)
     
-    def post(self, request):
-        serializer = ExpenseSerializer(data=request.data)
+#     def post(self, request):
+#         serializer = ExpenseSerializer(data=request.data)
 
-        if not serializer.is_valid():
-              return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         if not serializer.is_valid():
+#               return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 
-        expense = serializer.save()
+#         expense = serializer.save()
         
-        return Response(
-            ExpenseSerializer(expense).data,
-            status=status.HTTP_201_CREATED
-        )
+#         return Response(
+#             ExpenseSerializer(expense).data,
+#             status=status.HTTP_201_CREATED
+#         )
+    
+
+class ExpenseListCreateAPIView(ListCreateAPIView):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
