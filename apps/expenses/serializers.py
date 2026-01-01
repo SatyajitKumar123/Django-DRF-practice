16e1b82f180970f1d50ad5536bf7783dbbf3d34c
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-01 16:51:50 +0000

    Connected Expense model to serializer and added get method inside APIView

--- apps/expenses/serializers.py
+++ apps/expenses/serializers.py
@@ -1,38 +1,17 @@
 from rest_framework import serializers
 
-class ExpenseSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    amount = serializers.IntegerField()
-    category = serializers.CharField(max_length=50)
-    notes = serializers.CharField(
-        max_length=200,
-        required=False,
-        allow_blank=True,
-        write_only=True
-    )
-    summary = serializers.CharField(read_only=True)
-    
-    def validate_amount(self, value):
-        if value <= 0:
-            raise serializers.ValidationError(
-                "Amount must be greater than zero."
-            )
-        return value
-    
-    def validate(self, data):
-        if data.get("category") == "Food" and not data.get("notes"):
-            raise serializers.ValidationError({
-                "notes": "Notes are required for food expenses."
-            })
-        return data
-    
-    def to_representation(self, instance):
-        """
-        Controls OUTPUT only
-        """
-        return {
-            "title": instance["title"],
-            "amount": instance["amount"],
-            "category": instance["category"],
-            "summary": f'{instance["title"]} - ₹{instance["amount"]}'
-        }
+from .models import Expense
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = [
+            "id",
+            "title",
+            "amount",
+            "category",
+            "notes",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -2,10 +2,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from .models import Expense
 from .serializers import ExpenseSerializer
 
 
 class HealthAPIView(APIView):
+    def get(self, request):
+        expenses = Expense.objects.all()
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response(serializer.data)
+    
     def post(self, request):
         serializer = ExpenseSerializer(data=request.data)
 
@@ -13,4 +19,9 @@ class HealthAPIView(APIView):
               return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
 
-        return Response(serializer.data)
+        expense = serializer.save()
+        
+        return Response(
+            ExpenseSerializer(expense).data,
+            status=status.HTTP_201_CREATED
+        )
