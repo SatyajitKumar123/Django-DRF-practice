b7d75887722c54400458143d6521d5485aca4029
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-04 08:05:20 +0000

    Added user authentication and object-level permission

--- apps/expenses/models.py
+++ apps/expenses/models.py
@@ -1,12 +1,17 @@
+from django.conf import settings
 from django.db import models 
 
 
 class Expense(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="expense_items"
+    )
     title = models.CharField(max_length=100)
     amount = models.IntegerField()
     category = models.CharField(max_length=50)
     notes = models.TextField(blank=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- apps/expenses/views.py
+++ apps/expenses/views.py
@@ -1,16 +1,24 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
 
 from .models import Expense
 from .serializers import ExpenseSerializer
 
 
 class ExpenseListCreateAPIView(ListCreateAPIView):
-    queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
     
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
     
 class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
