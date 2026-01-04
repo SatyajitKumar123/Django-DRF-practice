ad0e9346ab36de8201ee7ad8ea453fa1bc5d5e5c
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-04 16:44:02 +0000

    Added api login endpoint 'obtain_auth_token', and tested using postman

--- apps/expenses/urls.py
+++ apps/expenses/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-# from .views import HealthAPIView
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import ExpenseListCreateAPIView, ExpenseDetailAPIView
 
 urlpatterns = [
     path("health/", ExpenseListCreateAPIView.as_view()),
     path("health/<int:pk>/", ExpenseDetailAPIView.as_view()),
+    
+    # "Login" endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
