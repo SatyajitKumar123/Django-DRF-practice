b66e5ed49e4fd3a70344f359e8e32973af8ed92a
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-01-05 15:53:50 +0000

    Added simple ViewSet for viewing and editing products, also added DefaultRouter, and tested all methods get, post, put, patch and delete using postman

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing products.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['sku', 'name']
+    ordering_fields = ['price', 'quantity']
+    
